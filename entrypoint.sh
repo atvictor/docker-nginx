d82c48206f301a5e6108d1c5556ac1e489a8c836
Victor <atvictor@users.noreply.github.com>
2015-08-15 18:52:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ $(ls -ld /data | awk '{print $3":"$4}') != "nginx:nginx" ]; then
-  chown -R nginx:nginx /data
+if [ $(ls -ld /nginx | awk '{print $3":"$4}') != "nginx:nginx" ]; then
+  chown -R nginx:nginx /nginx
 fi
 
 exec "$@"
